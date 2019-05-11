9450c24a159f009abfa359ad6c8c6298fe736d45
GatoY <derakhy@gmail.com>
2019-05-11 15:05:13 +1000

    server class

--- chat-room-GUI/server.py
+++ chat-room-GUI/server.py
@@ -3,60 +3,66 @@ from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 
 
-def accept_incoming_connections():
-    """Sets up handling for incoming clients."""
-    while True:
-        client, client_address = SERVER.accept()
-        print("%s:%s has connected." % client_address)
-        client.send(bytes("Welcome! Type your username and press enter to continue.", "utf8"))
-        addresses[client] = client_address
-        Thread(target=handle_client, args=(client,)).start()
+class Server:
+    def __init__(self):
+        self.clients = {}
+        self.addresses = {}
+        self.HOST = ''
+        self.PORT = 33000
+        self.BUFSIZ = 1024
+        self.ADDR = (self.HOST, self.PORT)
+
+    def start(self):
+        self.SERVER = socket(AF_INET, SOCK_STREAM)
+        self.SERVER.bind(self.ADDR)
+
+        self.SERVER.listen(5)
+        print("Waiting for connection...")
+        self.ACCEPT_THREAD = Thread(target=self.accept_incoming_connections)
+        self.ACCEPT_THREAD.start()
+        self.ACCEPT_THREAD.join()
+        self.SERVER.close()
+
+    def accept_incoming_connections(self):
+        """Sets up handling for incoming clients."""
+        while True:
+            client, client_address = self.SERVER.accept()
+            print("%s:%s has connected." % client_address)
+            client.send(bytes("Welcome! Type your username and press enter to continue.", "utf8"))
+            self.addresses[client] = client_address
+            Thread(target=self.handle_client, args=(client,)).start()
 
 
-def handle_client(client):  # Takes client socket as argument.
-    """Handles a single client connection."""
+    def handle_client(self, client):  # Takes client socket as argument.
+        """Handles a single client connection."""
 
-    name = client.recv(BUFSIZ).decode("utf8")
-    welcome = 'Welcome %s! If you want to quit, type {quit} to exit.' % name
-    client.send(bytes(welcome, "utf8"))
-    msg = "%s has joined the chat!" % name
-    broadcast(bytes(msg, "utf8"))
-    clients[client] = name
+        name = client.recv(self.BUFSIZ).decode("utf8")
+        welcome = 'Welcome %s! If you want to quit, type {quit} to exit.' % name
+        client.send(bytes(welcome, "utf8"))
+        msg = "%s has joined the chat!" % name
+        self.broadcast(bytes(msg, "utf8"))
+        self.clients[client] = name
 
-    while True:
-        msg = client.recv(BUFSIZ)
-        if msg != bytes("{quit}", "utf8"):
-            broadcast(msg, name + ": ")
-        else:
-            client.send(bytes("{quit}", "utf8"))
-            client.close()
-            del clients[client]
-            broadcast(bytes("%s has left the chat." % name, "utf8"))
-            break
-
-
-def broadcast(msg, prefix=""):  # prefix is for name identification.
-    """Broadcasts a message to all the clients."""
-
-    for sock in clients:
-        sock.send(bytes(prefix, "utf8") + msg)
+        while True:
+            msg = client.recv(self.BUFSIZ)
+            if msg != bytes("{quit}", "utf8"):
+                self.broadcast(msg, name + ": ")
+            else:
+                client.send(bytes("{quit}", "utf8"))
+                client.close()
+                del self.clients[client]
+                self.broadcast(bytes("%s has left the chat." % name, "utf8"))
+                break
 
 
-clients = {}
-addresses = {}
+    def broadcast(self, msg, prefix=""):  # prefix is for name identification.
+        """Broadcasts a message to all the clients."""
 
-HOST = ''
-PORT = 33000
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
+        for sock in self.clients:
+            sock.send(bytes(prefix, "utf8") + msg)
 
-SERVER = socket(AF_INET, SOCK_STREAM)
-SERVER.bind(ADDR)
+
 
 if __name__ == "__main__":
-    SERVER.listen(5)
-    print("Waiting for connection...")
-    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
-    ACCEPT_THREAD.start()
-    ACCEPT_THREAD.join()
-    SERVER.close()
+    server = Server()
+    server.start()
